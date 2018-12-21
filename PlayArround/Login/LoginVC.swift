2091573eb564fbe4689afb0af7fc14dcc71cc184
onurunal1 <onur.unal@smartmobilefactory.com>
2018-12-21 16:41:35 +0100

    First commit

---
+++ PlayArround/Login/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  PlayArround
+//
+//  Created by OnurMac on 21.12.18.
+//  Copyright © 2018 OnurMac. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Pods/Target Support Files/Pods-PlayArroundUITests/Pods-PlayArroundUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlayArroundUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PlayArroundUITests
+@end
